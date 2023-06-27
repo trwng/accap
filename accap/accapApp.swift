61fd6e1d33c149c53e9a0621e5fce4f733a3ba29
tara <tarawng18@gmail.com>
2023-06-27 09:10:12 -0400

    Initial Commit

---
+++ accap/accapApp.swift
@@ -0,0 +1,17 @@
+//
+//  accapApp.swift
+//  accap
+//
+//  Created by scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct accapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ accapUITests/accapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  accapUITestsLaunchTests.swift
+//  accapUITests
+//
+//  Created by scholar on 6/27/23.
+//
+
+import XCTest
+
+final class accapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
